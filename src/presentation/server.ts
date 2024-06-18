7de27553ec8dba6cf4cef36e87643ad6e967c7ca
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-17 22:59:36 -0400

    chore: ✨ add cron dependency for scheduled tasks

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,16 @@
+import { CronJob } from 'cron'
+
 export class Server {
   public static start() {
     console.log('Server started...')
+    const job = new CronJob(
+      '*/2 * * * * *', // cronTime
+      function () {
+        const today = new Date()
+        console.log(`2 second'`, today)
+      }, // onTick
+     
+    )
+    job.start()
   }
 }
