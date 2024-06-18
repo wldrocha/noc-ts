f879066444165777fcf68cee4ec7ce67ea502df3
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-17 23:46:36 -0400

    chore: ♻️ Add success and error callbacks to CheckService constructor

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -6,8 +6,12 @@ export class Server {
     console.log('Server started...')
 
     const job = CronService.createJob('*/5 * * * * *', () => {
+      const url = 'https://google.com'
       const date = new Date()
-      new CheckService().execute('https://google.com')
+      new CheckService(
+        () => console.log(`✅ ${url} - CheckService is ok`),
+        (error) => console.log(`❌ ${date} - ${error}`)
+      ).execute(url)
     })
   }
 }
