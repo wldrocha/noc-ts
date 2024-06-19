1278928c6dbe78ba248370892d4bcc03cb5834f4
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-18 23:22:01 -0400

    chore:  🚧  Update log file paths in FileSystemDataSource

--- src/infrastructure/datasources/file-system.datasource.ts
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -4,16 +4,16 @@ import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity'
 
 export class FileSystemDataSource implements LogDataSource {
   private readonly logPath = 'logs/'
-  private readonly lowLogsPath = 'logs/low-logs.log'
-  private readonly mediumLogsPath = 'logs/medium-logs.log'
-  private readonly hightLogsPath = 'logs/hight-logs.log'
+  private readonly allLogsPath = 'logs/logs-all.log'
+  private readonly mediumLogsPath = 'logs/logs-medium.log'
+  private readonly hightLogsPath = 'logs/logs-hight.log'
 
   constructor() {
     this.createLogsFiles()
   }
 
   private createLogsFiles = () => {
-    const allPaths = [this.lowLogsPath, this.mediumLogsPath, this.hightLogsPath]
+    const allPaths = [this.allLogsPath, this.mediumLogsPath, this.hightLogsPath]
     if (!fs.existsSync(this.logPath)) {
       fs.mkdirSync(this.logPath)
     }
@@ -25,10 +25,39 @@ export class FileSystemDataSource implements LogDataSource {
     })
   }
 
-  saveLog(log: LogEntity): Promise<void> {
-    throw new Error('Method not implemented.')
+  async saveLog(newLog: LogEntity): Promise<void> {
+    const logToJson = `${JSON.stringify(newLog)}\n`
+
+    fs.appendFileSync(this.allLogsPath, logToJson)
+    if (newLog.level === LogSeverityLevel.low) return
+
+    if (newLog.level === LogSeverityLevel.medium) {
+      fs.appendFileSync(this.mediumLogsPath, logToJson)
+    } else {
+      fs.appendFileSync(this.hightLogsPath, logToJson)
+    }
   }
   getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
     throw new Error('Method not implemented.')
   }
+
+  //   private getLogPath = (level: LogSeverityLevel) => {
+  //     const levels = {
+  //       [LogSeverityLevel.low]: this.allLogsPath,
+  //       [LogSeverityLevel.medium]: this.mediumLogsPath,
+  //       [LogSeverityLevel.high]: this.hightLogsPath
+  //     }
+
+  //     return levels[level] ?? this.allLogsPath
+  //   }
+
+  //   async saveLog(newLog: LogEntity): Promise<void> {
+  //     const logToJson = `${JSON.stringify(newLog)}\n`
+
+  //     fs.appendFileSync(this.allLogsPath, logToJson)
+  //     if (newLog.level === LogSeverityLevel.low) return
+
+  //     const logPath = this.getLogPath(newLog.level)
+  //     fs.appendFileSync(logPath, logToJson)
+  //   }
 }
