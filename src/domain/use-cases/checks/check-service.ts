64de41e55a5df00799ef5e709f22a49df18f44b3
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-19 21:30:43 -0400

    chore: 🚧 Update CheckService to log success, error messages and save log

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,3 +1,6 @@
+import { LogEntity, LogSeverityLevel } from '../../entities/log.entity'
+import { LogRepository } from '../../repository/log.repository'
+
 interface CheckServiceUseCase {
   execute(url: string): Promise<boolean>
 }
@@ -7,17 +10,25 @@ type ErrorCallback = (error: string) => void
 
 export class CheckService implements CheckServiceUseCase {
   constructor(
+    private readonly logRepository: LogRepository,
     private readonly successCallback: SuccessCallback,
-    private readonly errorCallback: ErrorCallback) {}
+    private readonly errorCallback: ErrorCallback
+  ) {}
   public async execute(url: string): Promise<boolean> {
     try {
       const req = await fetch(url)
       if (!req.ok) {
         throw new Error(`Error on check service ${url}`)
       }
+
+      const log = new LogEntity(LogSeverityLevel.low, `Check service ${url} is OK`)
+      this.logRepository.saveLog(log)
       this.successCallback()
       return true
     } catch (error) {
+      const errorMessage = `${url} is not ok, ${error}`
+      const log = new LogEntity(LogSeverityLevel.high, errorMessage)
+      this.logRepository.saveLog(log)
       this.errorCallback(`${error}`)
       return false
     }
