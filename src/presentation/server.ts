1f06fdd4894028071d838f564611b2205398cab6
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-19 22:22:06 -0400

    refactor: ♻️ Update CheckService to use FileSystemDataSource for logging

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,14 +1,20 @@
 import { CheckService } from '../domain/use-cases/checks/check-service'
+import { FileSystemDataSource } from '../infrastructure/datasources/file-system.datasource'
+import { LogRepositoryImpl } from '../infrastructure/repositories/log.respostory.impl'
 import { CronService } from './service/cron-service'
+
+const fileSystemLogRepository = new LogRepositoryImpl(new FileSystemDataSource())
 
 export class Server {
   public static start() {
     console.log('Server started...')
 
     const job = CronService.createJob('*/5 * * * * *', () => {
-      const url = 'https://google.com'
+      // const url = 'https://google.com'
+      const url = 'https://localhost:3000'
       const date = new Date()
       new CheckService(
+        fileSystemLogRepository,
         () => console.log(`✅ ${url} - CheckService is ok`),
         (error) => console.log(`❌ ${date} - ${error}`)
       ).execute(url)
