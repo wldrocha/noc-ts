9d3cb9ad2fa12cef823bdf411a6a6cf57c546784
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-20 13:00:00 -0400

    chore: 🚧 Add MAILER_SERVICE environment variable to env.plugins.ts

--- src/config/plugins/env.plugins.ts
+++ src/config/plugins/env.plugins.ts
@@ -3,5 +3,6 @@ import * as env from 'env-var'
 
 export const envs = {
   MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
-  MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString()
+  MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
+  MAILER_SERVICE: env.get('MAILER_SERVICE').required().asString()
 }
