0bca2dca9c880b828b1aabb522f625472df12b98
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-20 20:54:44 -0400

    feat: ✨ Add MongoDB connection initialization

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
+import { envs } from './config/plugins/env.plugins'
+import { MongoDatabase } from './data/mongo'
 import { Server } from './presentation/server'
-
 ;(async () => {
-    main()
+  main()
 })()
 
-function main() {
+async function main() {
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME
+  })
   Server.start()
 }
