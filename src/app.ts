5eb17568aae103b4234b3ed62edeaa3e60c7f7b3
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-20 21:11:35 -0400

    chore: 🚧 Add Log model for MongoDB logging

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { envs } from './config/plugins/env.plugins'
-import { MongoDatabase } from './data/mongo'
+import { MongoDatabase, logModel } from './data/mongo'
 import { Server } from './presentation/server'
 ;(async () => {
   main()
@@ -10,5 +10,18 @@ async function main() {
     mongoUrl: envs.MONGO_URL,
     dbName: envs.MONGO_DB_NAME
   })
-  Server.start()
+
+  // const newLog = await logModel.create({
+  //   message: 'Test message to mongo',
+  //   level: 'low',
+  //   origin: 'app.ts'
+  // })
+
+  // await newLog.save()
+
+  // console.log("🚀 ~ main ~ newLog:", newLog)
+
+  const logs = await logModel.find()
+  console.log('🚀 ~ main ~ logs', logs)
+  // Server.start()
 }
