9e71c85ffdd5ef59c9a26fe3687b18a3d95571ea
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-20 21:11:53 -0400

    refactor: ♻️ Update MongoDB connection configuration in env.plugins.ts

--- src/config/plugins/env.plugins.ts
+++ src/config/plugins/env.plugins.ts
@@ -6,7 +6,7 @@ export const envs = {
   MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
   MAILER_SERVICE: env.get('MAILER_SERVICE').required().asString(),
   MONGO_URL: env.get('MONGO_URL').required().asUrlString(),
-  MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asUrlString(),
-  MONGO_USER: env.get('MONGO_USER').required().asUrlString(),
-  MONGO_PASS: env.get('MONGO_PASS').required().asUrlString()
+  MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asString(),
+  MONGO_USER: env.get('MONGO_USER').required().asString(),
+  MONGO_PASS: env.get('MONGO_PASS').required().asString()
 }
