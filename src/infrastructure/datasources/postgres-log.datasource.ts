1de470e933f8c8b3c40a4736d36a543513510013
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-22 12:40:51 -0400

    refactor: Update log entity parsing and severity level handling
    teacher implementation

--- src/infrastructure/datasources/postgres-log.datasource.ts
+++ src/infrastructure/datasources/postgres-log.datasource.ts
@@ -4,11 +4,17 @@ import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity'
 
 const prismaClient = new PrismaClient()
 
+const severityEnum ={
+    low: SeverityLevel.LOW,
+    medium: SeverityLevel.MEDIUM,
+    high: SeverityLevel.HIGH
+}
+
 export class PostgresLogDataSource implements LogDataSource {
   async saveLog(log: LogEntity): Promise<void> {
     await prismaClient.logModel.create({
       data: {
-        level: log.level.toUpperCase() as SeverityLevel,
+        level: severityEnum[log.level],
         message: log.message,
         origin: log.origin
       }
@@ -17,9 +23,9 @@ export class PostgresLogDataSource implements LogDataSource {
   async getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
     const logs = await prismaClient.logModel.findMany({
       where: {
-        level: severityLevel.toUpperCase() as SeverityLevel
+        level: severityEnum[severityLevel]
       }
     })
-    return logs.map((log) => LogEntity.fromObject(log))
+    return logs.map(LogEntity.fromObject)
   }
 }
