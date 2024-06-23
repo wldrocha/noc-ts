b17c1cc004e5664193e4bb0ce5956a506732ecbd
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-23 09:00:02 -0400

    feat: ✨ check service with multiply save

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,6 +1,7 @@
 import { envs } from '../config/plugins/env.plugins'
 import { LogSeverityLevel } from '../domain/entities/log.entity'
 import { CheckService } from '../domain/use-cases/checks/check-service'
+import { CheckServiceMultiple } from '../domain/use-cases/checks/check-service-multiple'
 import { SendEMailLogs } from '../domain/use-cases/email/send-logs'
 import { FileSystemDataSource } from '../infrastructure/datasources/file-system.datasource'
 import { MongoLogDataSource } from '../infrastructure/datasources/mongo-log.datasource'
@@ -9,44 +10,32 @@ import { LogRepositoryImpl } from '../infrastructure/repositories/log.respostory
 import { CronService } from './service/cron-service'
 import { EmailService } from './service/email.service'
 
-const logRepository = new LogRepositoryImpl(
-  // new FileSystemDataSource()
-  // new MongoLogDataSource()
-  new PostgresLogDataSource()
-)
+const fsLogRepository = new LogRepositoryImpl(new FileSystemDataSource())
+const mongoLogRepository = new LogRepositoryImpl(new MongoLogDataSource())
+const postgresLogRepository = new LogRepositoryImpl(new PostgresLogDataSource())
 const emailService = new EmailService()
 
 export class Server {
   public static async start() {
     console.log('Server started...')
 
-    // new SendEMailLogs(emailService, logRepository).execute(['wld.rocha@gmail.com', 'wrocha@grupov.com.ve'])
-
-    // emailService.sendEmail({
-    //   to: 'wrocha@grupov.com.ve',
-    //   subject: 'Test email',
-    //   htmlBody: `
-    //   <h1>Test email</h1>
-    //   <p>Lorem impusm kdlfaklsdfkldklasfkladklsfklds</p>
-    //   `
-    // })
 
     // const isSendEmail = await emailService.sendEmailWithFileSystemLog(['wld.rocha@gmail.com', 'wrocha@grupov.com.ve'])
     // console.log('🚀 ~ Server ~ start ~ isSendEmail:', isSendEmail)
-    const logs = await logRepository.getLogs(LogSeverityLevel.medium)
-    console.log("🚀 ~ Server ~ start ~ logs:", logs)
-    // const job = CronService.createJob('*/5 * * * * *', () => {
-    //   const url = 'https://google.com'
-    //   // const url = 'https://localhost:3000'
-    //   const date = new Date()
-    //   new CheckService(
-    //     logRepository,
-    //     // undefined,
-    //     // undefined
-    //     () => console.log(`✅ ${url} - CheckService is ok`),
+    // const logs = await logRepository.getLogs(LogSeverityLevel.medium)
+    // console.log("🚀 ~ Server ~ start ~ logs:", logs)
+    const job = CronService.createJob('*/5 * * * * *', () => {
+      // const url = 'https://google.com'
+      const url = 'https://localhost:3000'
+      const date = new Date()
+      new CheckServiceMultiple(
+        [fsLogRepository, mongoLogRepository, postgresLogRepository],
+        // undefined,
+        // undefined
+        () => console.log(`✅ ${url} - CheckService is ok`),
 
-    //     (error) => console.log(`❌ ${date} - ${error}`)
-    //   ).execute(url)
-    // })
+        (error) => console.log(`❌ ${date} - ${error}`)
+      ).execute(url)
+    })
   }
 }
