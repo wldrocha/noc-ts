3104e36ea95b82bc891e8621c3b1e8714249a76f
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-24 19:18:24 -0400

    chore: ♻️ Update MongoLogDataSource to log saved log ID

--- src/infrastructure/datasources/mongo-log.datasource.ts
+++ src/infrastructure/datasources/mongo-log.datasource.ts
@@ -4,7 +4,8 @@ import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity'
 
 export class MongoLogDataSource implements LogDataSource {
   async saveLog(log: LogEntity): Promise<void> {
-    await logModel.create(log)
+    const savedLog = await logModel.create(log)
+    console.log("🚀 ~ MongoLogDataSource ~ saveLog ~ savedLog:", savedLog.id)
   }
   async getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
     const logs = await logModel.find({ level: severityLevel })
